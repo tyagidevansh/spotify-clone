c5ab50c218e5d18426a902c05f29c344f6683fb3
tyagidevansh <devanshtyagi0607@gmail.com>
2024-04-16 18:21:50 +0530

    home page buttons

--- index.php
+++ index.php
@@ -16,8 +16,76 @@ if (isset($_SESSION['userLoggedIn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Musify</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
+
 <body>
+    <div id = "nowPlayingBarContainer">
     
+        <div id = "nowPlayingBar">
+            
+            <div id = "nowPlayingLeft">
+
+
+            </div>
+
+            <div id = "nowPlayingCenter">
+                
+                <div class = "content playerControls">
+
+                    <div class = "buttons" >
+
+                        <button class = "controlButton shuffle" title = "Shuffle button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+
+                        <button class = "controlButton previous" title = "Previous button">
+                            <img src="assets/images/icons/previous.png" alt="Previous song">
+                        </button>
+
+                        <button class="controlButton play" title="Play button" onclick="togglePlayPause()">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+
+                        <button class="controlButton pause" title="Pause button" style="display: none;" onclick="togglePlayPause()">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+
+                        <script>
+                            function togglePlayPause() {
+                                var playButton = document.querySelector('.controlButton.play');
+                                var pauseButton = document.querySelector('.controlButton.pause');
+
+                                if (playButton.style.display !== 'none') {
+                                    playButton.style.display = 'none';
+                                    pauseButton.style.display = 'inline-block';
+                                } else {
+                                    playButton.style.display = 'inline-block';
+                                    pauseButton.style.display = 'none';
+                                }
+                            }
+                        </script>
+
+                        <button class = "controlButton next" title = "Next button">
+                            <img src="assets/images/icons/next.png" alt="Next song">
+                        </button>
+
+                        <button class = "controlButton repeat" title = "Repeat button">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div id = "nowPlayingRight">
+                
+
+            </div>
+        </div>
+
+    </div>
+
 </body>
 </html>
--- register.php
+++ register.php
@@ -13,6 +13,7 @@
         }
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,83 +25,108 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src = "assets/js/register.js"></script>
+
 </head>
 <body>
-    <script>
-        // $(document).ready(function() {
-        //     $("#loginForm").show();
-        //     $("#registerForm").hide();
-        // });
-    </script>
+    <?php
+    if (isset($_POST['registerButton'])) {
+        echo '<script>
+                $(document).ready(function() {
+                    $("#loginForm").hide();
+                    $("#registerForm").show();
+                });
+            </script>';
+    } else {
+        echo '<script>
+                $(document).ready(function() {
+                    $("#loginForm").show();
+                    $("#registerForm").hide();
+                });
+            </script>';
+    }
+    ?>
+
     <div id = "background">
-        <div id = "inputContainer">
-            <form id = "loginForm" action="register.php" method="post">
-                <h2>Login to your account</h2>
-                <p>
-                    <?php echo $account->getError(Constants::$loginFailed); ?>
-                    <label for="loginUsername">Username</label>
-                    <input id = "loginUsername" name = "loginUsername" type="text" placeholder = "John Doe" required>
-                </p>
-                <p>
-                    <label for="loginPassword">Password</label>
-                    <input id = "loginPassword" name = "loginPassword" type="password" placeholder = "Your password" required>
-                </p>
-                <button type = "submit" name = "loginButton" > LOG IN</button>
+        <div id = "loginContainer">
+            <div id = "inputContainer">
+                <form id = "loginForm" action="register.php" method="post">
+                    <h2>Login to your account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$loginFailed); ?>
+                        <label for="loginUsername">Username</label>
+                        <input id = "loginUsername" name = "loginUsername" type="text" placeholder = "John Doe" value = "<?php getInputValue('loginUsername')?>" required>
+                    </p>
+                    <p>
+                        <label for="loginPassword">Password</label>
+                        <input id = "loginPassword" name = "loginPassword" type="password" placeholder = "Your password" required>
+                    </p>
+                    <button type = "submit" name = "loginButton" > LOG IN</button>
 
-                <div class = "hasAccountText">
-                    <span id = "hideLogin"> Don't have an account yet? Signup here. </span>
-                </div>
-            </form>
+                    <div class = "hasAccountText">
+                        <span id = "hideLogin"> Don't have an account yet? Signup here. </span>
+                    </div>
+                </form>
 
 
-            <form id = "registerForm" action="register.php" method="post">
-                <h2>Create your free account</h2>
-                <p>
-                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
-                    <?php echo $account->getError(Constants::$usernameTaken); ?>
-                    <label for="username">Username</label>
-                    <input id = "username" name = "username" type="text" placeholder = "eg John Doe" value = "<?php getInputValue('username')?>" required>
-                </p>
-                <p>
-                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
-                    <label for="firstName">First name</label>
-                    <input id = "firstName" name = "firstName" type="text" placeholder = "eg John" value = "<?php getInputValue('firstName')?>" required>
-                </p>
-                <p>
-                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
-                    <label for="lastName">Last name</label>
-                    <input id = "lastName" name = "lastName" type="text" placeholder = "eg Doe" value = "<?php getInputValue('lastName')?>" required>
-                </p>
-                <p>
-                    <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-                    <?php echo $account->getError(Constants::$emailInvalid); ?>
-                    <?php echo $account->getError(Constants::$emailTaken); ?>
-                    <label for="email">Email</label>
-                    <input id = "email" name = "email" type="email" placeholder = "eg johndoe420@mail.com" value = "<?php getInputValue('email')?>" required>
-                </p>
-                <p>
-                    <label for="email2">Confirm Email</label>
-                    <input id = "email2" name = "email2" type="text" placeholder = "eg johndoe420@mail.com" value = "<?php getInputValue('email2')?>" required>
-                </p>
+                <form id = "registerForm" action="register.php" method="post">
+                    <h2>Create your free account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                        <?php echo $account->getError(Constants::$usernameTaken); ?>
+                        <label for="username">Username</label>
+                        <input id = "username" name = "username" type="text" placeholder = "eg John Doe" value = "<?php getInputValue('username')?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                        <label for="firstName">First name</label>
+                        <input id = "firstName" name = "firstName" type="text" placeholder = "eg John" value = "<?php getInputValue('firstName')?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                        <label for="lastName">Last name</label>
+                        <input id = "lastName" name = "lastName" type="text" placeholder = "eg Doe" value = "<?php getInputValue('lastName')?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                        <?php echo $account->getError(Constants::$emailInvalid); ?>
+                        <?php echo $account->getError(Constants::$emailTaken); ?>
+                        <label for="email">Email</label>
+                        <input id = "email" name = "email" type="email" placeholder = "eg johndoe420@mail.com" value = "<?php getInputValue('email')?>" required>
+                    </p>
+                    <p>
+                        <label for="email2">Confirm Email</label>
+                        <input id = "email2" name = "email2" type="text" placeholder = "eg johndoe420@mail.com" value = "<?php getInputValue('email2')?>" required>
+                    </p>
 
-                <p>
-                    <?php echo $account->getError(Constants::$passwordsCharacters); ?>
-                    <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-                    <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
-                    <label for="password">Password</label>
-                    <input id = "password" name = "password" type="password" placeholder = "Your password" value = "<?php getInputValue('password')?>" required>
-                </p>
-                <p>
-                    <label for="password2">Confirm Password</label>
-                    <input id = "password2" name = "password2" type="password" placeholder = "Your password" value = "<?php getInputValue('password2')?>" required>
-                </p>
-                <button type = "submit" name = "registerButton" >SIGN UP</button>
+                    <p>
+                        <?php echo $account->getError(Constants::$passwordsCharacters); ?>
+                        <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                        <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                        <label for="password">Password</label>
+                        <input id = "password" name = "password" type="password" placeholder = "Your password" value = "<?php getInputValue('password')?>" required>
+                    </p>
+                    <p>
+                        <label for="password2">Confirm Password</label>
+                        <input id = "password2" name = "password2" type="password" placeholder = "Your password" value = "<?php getInputValue('password2')?>" required>
+                    </p>
+                    <button type = "submit" name = "registerButton" >SIGN UP</button>
 
-                <div class = "hasAccountText">
-                    <span id = "hideRegister"> Already have an account? Login here. </span>
-                </div>
-            
-            </form>
+                    <div class = "hasAccountText">
+                        <span id = "hideRegister"> Already have an account? Login here. </span>
+                    </div>
+                
+                </form>
+            </div>
+
+            <div id = "loginText">
+                <h1> Get great music, anytime, anywhere </h1>
+                <h2> Listen to millions of songs for free </h1>
+                <ul>
+                    <li>Discover music you'll fall in love with</li>
+                    <li>Create your own playlists</li>
+                    <li>Follow artists and communities</li>
+                </ul>
+            </div>
         </div>
     </div>
 </body>
